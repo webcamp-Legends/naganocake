dd4eb07c2254a5a60ab286f58c598ef370f67583
kome44733 <110373715+kome44733@users.noreply.github.com>
2022-09-18 03:09:29 +0000

    first

--- db/schema.rb
+++ db/schema.rb
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 2022_09_17_084334) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name", default: "", null: false
+    t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
