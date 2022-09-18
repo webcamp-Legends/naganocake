e45f0e1bec75909f24166a5995fe226e78753342
kome44733 <110373715+kome44733@users.noreply.github.com>
2022-09-18 09:14:03 +0000

    変更中

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :last_name, presence: true        ,length: { in: 1..15 }
+  validates :first_name, presence: true       ,length: { in: 1..15 }
+  validates :last_name_kana, presence: true   ,length: { in: 1..15 }
+  validates :first_name_kana, presence: true  ,length: { in: 1..15 }
+  validates :post_code, presence: true        ,length: { is: 7 }
+  validates :address, presence: true          ,length: { in: 1..15 }
+  validates :phone_number, presence: true     ,length: { in: 1..15 }
+  
 end
