adaeebbbb7f826cdf135d29f8b0d5eb539272eae
EC2 Default User <110474164+Jupiter200108@users.noreply.github.com>
2022-09-18 09:11:24 +0000

    Add enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum order_status: { payment_waiting: 0, payment_confirmation: 1, in_production:2, preparing_delivery:3, delivered:4 }
 end
