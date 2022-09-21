9bfe2402cf800383276d17e4fe649a851518156c
kome44733 <110373715+kome44733@users.noreply.github.com>
2022-09-21 05:17:11 +0000

    レイアウト

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -32,7 +32,7 @@ class Public::AddressesController < ApplicationController
   def destroy
     @address = Address.find(params[:id])
     @address.destroy
-    redirect_to addresses_path
+    redirect_to books_path
   end
 
   private
