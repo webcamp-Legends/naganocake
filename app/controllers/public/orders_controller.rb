75d3dcdcd997e786222c7e32d0da42395402c6e5
itigoame <rikuan1128@gmail.com>
2022-09-21 11:01:45 +0000

    [add]seach genre

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -15,7 +15,29 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    @order = Order.find(params[:id])
+    @order = Order.new(order_params)
+
+    if params [:order][:delivery_target] == 0
+    @order.post_code = current_customer.post_code
+    @order.address = current_customer.address
+    @order.last_name = current_customer.name
+    @order.first_name = current_customer.name
+    @order.shipping_cost = shipping_cost
+    @order.total_payment = current_customer.cart_items.total_payment
+
+    elsif params [:order][:delivery_target] == 1
+
+    @order.post_code = current_customer.post_code
+    @order.address = current_customer.address
+    @order.name = current_customer.name
+
+    else
+
+    @order.post_code = current_customer.post_code
+    @order.address = current_customer.address
+    @order.name = current_customer.name
+
+    end
   end
 
   def create
