3afd236e9ad3d996a5302a37b7faf8e8e0a2ceef
itigoame <rikuan1128@gmail.com>
2022-09-22 05:08:46 +0000

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,6 @@ Rails.application.routes.draw do
     resources :cart_items,only:[:create, :index, :update, :destroy]
     get '/cart_items/destroy_all' => "cart_items#destroy_all"
 
-    
     resources :orders,only:[:create, :index, :new, :edit, :update,:show]
     get "/orders/confirm" =>"orders#confirm"
     get "/orders/thanks" =>"orders#thanks"
