c88c75c0b548084727ce515a82423155ef72b14b
itigoame <rikuan1128@gmail.com>
2022-09-22 05:37:50 +0000

    check_schema

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 2022_09_17_084334) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "name", null: false
+    t.string "post_code", null: false
+    t.string "address", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_addresses_on_customer_id"
+  end
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
