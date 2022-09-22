d4b7d5b8b53fa08700efee840acfbafd2488f128
itigoame <rikuan1128@gmail.com>
2022-09-22 09:52:09 +0000

    design

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,10 +2,12 @@ class Public::ItemsController < ApplicationController
   def index
     @items_search = Item.where(is_active: true)
     @items = @items_search.page(params[:page]).per(8)
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
 end
