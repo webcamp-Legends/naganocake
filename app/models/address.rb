05bd8c7049184151b84f95e70c3e43c7df4f3c09
kome44733 <110373715+kome44733@users.noreply.github.com>
2022-09-22 07:40:26 +0000

    配送先レイアウト

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :customer
-  validates :postcode, presence: true        ,length: { is: 7 }
+  validates :post_code, presence: true        ,length: { is: 7 }
   validates :address, presence: true          ,length: { in: 1..15 }
   validates :name, presence: true        ,length: { in: 1..15 }
 end
