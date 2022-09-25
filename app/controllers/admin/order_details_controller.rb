11ff9d6fdeb634f211715ba447bc10be3cd2b94d
EC2 Default User <110474164+Jupiter200108@users.noreply.github.com>
2022-09-25 03:31:34 +0000

    add status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,18 +1,14 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-     @order_details=OrderDetail.find(params[:id])
-     @order= @order_details.order
-    if
-      @order_details.update(order_details_params)
-      if @order.order_details.any?{|order_detail| order_detail.making_status == "製作中"}
-        @order.update(order_status: "製作中")
+    @order_details=OrderDetail.find(params[:id])
+    @order= @order_details.order
+    if@order_details.update(order_details_params)
+      if @order.order_details.any?{|order_detail| order_detail.making_status == "in_production"}
+        @order.update(order_status: "in_production")
+      elsif@order.order_details.all?{|order_detail| order_detail.making_status == "completed"}
+        @order.update(order_status: "preparing_delivery")
       end
-      if @order.order_details.all?{|order_detail| order_detail.making_status == "製作完了"}
-        @order.update(order_status: "発送準備中")
-      end
-      redirect_to admin_order_path(@order_details.order)
-    else
-      render admin_order_path
+      redirect_to admin_order_path(@order)
     end
   end
   
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,9 +11,14 @@ class Admin::OrdersController < ApplicationController
   def update
     @order= Order.find(params[:id])
     @order_details= @order.order_details
-
-    @order.update(order_params)
-    redirect_to admin_order_path(@order)
+    if @order.update(order_params)
+      if @order.order_status == "payment_confirmation"
+        @order.order_details.each do |order_detail|
+          order_detail.update(making_status: "production_waiting")
+        end
+      end
+      redirect_to admin_order_path(@order.id)
+    end
   end
 
   private
